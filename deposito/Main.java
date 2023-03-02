b837cb9cdd25db9aba502eed1bc9cc149bb1c462
tochene <a19antonioma@iessancl.net>
2023-03-02 10:55:26 +0100

    first commit

---
+++ deposito/Main.java
@@ -0,0 +1,31 @@
+package deposito;
+
+public class Main {
+
+    public static void main(String[] args) {
+        CCuenta cuenta1;
+        double saldoActual;
+        float cantidad;
+
+        cuenta1 = new CCuenta("Antonio López", "1000-2365-85-1230456789", 2500, 0);
+        saldoActual = cuenta1.estado();
+        cantidad = 2300;
+        System.out.println("El saldo actual es " + saldoActual);
+
+        operativa_cuenta(cuenta1, cantidad);
+    }
+
+    private static void operativa_cuenta(CCuenta cuenta1, float cantidad) {
+        try {
+            cuenta1.retirar(cantidad);
+        } catch (Exception e) {
+            System.out.print("Fallo al retirar");
+        }
+        try {
+            System.out.println("Ingreso en cuenta");
+            cuenta1.ingresar(cantidad);
+        } catch (Exception e) {
+            System.out.print("Fallo al ingresar");
+        }
+    }
+}
